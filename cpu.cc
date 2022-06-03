69fed57789183e61f95bdb77fdbf37635a683d6a
arthurwinck <apwinck@gmail.com>
2022-06-03 19:08:14 -0300

    V1.0 Funcionando e sem Vazamentos de Memória Amém

--- cpu.cc
+++ cpu.cc
@@ -22,9 +22,6 @@ CPU::Context::~Context()
 {
     //adicionar implementação
     //chamado quando o contexto é deletado no final de main
-    //free(_context.uc_stack.ss_sp); //remove alocação para stack
-    //free(_context.uc_link); //remove valor para executar ao retornar de makecontext
-
     //Update para a destruição da stack
     if (this->_stack) {
         delete this->_stack;
